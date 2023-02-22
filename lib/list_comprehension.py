1c3fdf64acb0b996f59b51cc344062f2bf9455ba
Areeb Karim <areebakarim99@gmail.com>
2023-02-22 11:13:12 -0500

    done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 
+# Areeb Solution
 def return_evens(num_list):
-    pass
+    list_evens = [i for i in num_list if (i % 2 == 0)]
+    return list_evens
+
+# Main Solution
+# def return_evens(num_list):
+#     return [n for n in num_list if n%2 == 0]
+    
+
+
+
 
 def make_exclamation(sentence_list):
-    pass
+    return [str + "!" for str in sentence_list]
+
+
+# IT WORKS
